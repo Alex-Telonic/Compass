09491c4f6e466b633b9d224a437527af29e83765
unknown <AlexanderSchlechter@aschlechter.telonic.de>
2019-04-24 14:43:35 +0200

    Compass implemented

---
+++ app/src/main/java/com/aschlechter/compass/MainActivity.java
@@ -0,0 +1,215 @@
+package com.aschlechter.compass;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private static final String TAG = "MainActivity";
+
+
+    /* Vibration Service */
+    private static final UUID VIBRATION_SERVICE = UUID.fromString("713d0000-503e-4c75-ba94-3148f18d941e");
+    private static final UUID VIBRATION_CHARACTERISTIC = UUID.fromString("713d0003-503e-4c75-ba94-3148f18d941e");
+    private static final int REQUEST_ENABLE_BT = 1;
+
+    private BluetoothAdapter bluetoothAdapter;
+    private SparseArray<BluetoothDevice> mDevices;
+    private BluetoothGatt mConnectedGatt;
+    private ProgressDialog mProgress;
+
+    ImageView compass_img;
+    TextView txt_compass;
+    int mAzimuth;
+    private SensorManager mSensorManager;
+    private Sensor mRotationV, mAccelerometer, mMagnetometer;
+    boolean haveSensor = false, haveSensor2 = false;
+    float[] rMat = new float[9];
+    float[] orientation = new float[3];
+    private float[] mLastAccelerometer = new float[3];
+    private float[] mLastMagnetometer = new float[3];
+    private boolean mLastAccelerometerSet = false;
+    private boolean mLastMagnetometerSet = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        compass_img = (ImageView) findViewById(R.id.img_compass);
+        txt_compass = (TextView) findViewById(R.id.txt_azimuth);
+
+        
+
+        /*View.OnClickListener startListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start();
+            }
+        };
+        */
+
+        // Initializes Bluetooth adapter.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+
+        /*
+        mProgress = new ProgressDialog(this);
+        mProgress.setIndeterminate(true);
+        mProgress.setCancelable(false);
+        */
+
+
+    }
+
+    public void stop() {
+        if (haveSensor && haveSensor2) {
+            mSensorManager.unregisterListener(this, mAccelerometer);
+            mSensorManager.unregisterListener(this, mMagnetometer);
+        } else {
+            if (haveSensor)
+                mSensorManager.unregisterListener(this, mRotationV);
+        }
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //mHandler.removeCallbacks(mStopRunnable);
+
+        stop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            finish();
+            return;
+        }
+
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "No LE Support", Toast.LENGTH_SHORT);
+            finish();
+            return;
+        }
+
+        start();
+    }
+
+    private void start() {
+        Log.d(TAG, "start: reading in sensor values");
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR) == null) { //try to get Rotation Vector
+            if ((mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) == null) //if there is not rotation vector, try to get accelerometer and magnetic field
+                    || (mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) == null)) {
+                noSensorsAlert();//no sensors available
+            } else {
+                mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+                haveSensor = mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+                haveSensor2 = mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_UI);
+            }
+        } else {
+            mRotationV = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+            haveSensor = mSensorManager.registerListener(this, mRotationV, SensorManager.SENSOR_DELAY_UI);
+        }
+        Log.d(TAG, "start: finished reading in sensor values");
+    }
+
+
+    private void noSensorsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setMessage("Your device doesn't support the Compass.")
+                .setCancelable(false)
+                .setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+        alertDialog.show();
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            SensorManager.getRotationMatrixFromVector(rMat, event.values); //create rotation matrix using rotate vector values
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
+            mLastAccelerometerSet = true;
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, mLastMagnetometer, 0, event.values.length);
+            mLastMagnetometerSet = true;
+        }
+        if (mLastAccelerometerSet && mLastMagnetometerSet) {
+            SensorManager.getRotationMatrix(rMat, null, mLastAccelerometer, mLastMagnetometer);
+            SensorManager.getOrientation(rMat, orientation);
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
+
+        mAzimuth = Math.round(mAzimuth);
+        compass_img.setRotation(-mAzimuth);
+
+        String where = "NW";
+
+        if (mAzimuth >= 350 || mAzimuth <= 10)
+            where = "N";
+        if (mAzimuth < 350 && mAzimuth > 280)
+            where = "NW";
+        if (mAzimuth <= 280 && mAzimuth > 260)
+            where = "W";
+        if (mAzimuth <= 260 && mAzimuth > 190)
+            where = "SW";
+        if (mAzimuth <= 190 && mAzimuth > 170)
+            where = "S";
+        if (mAzimuth <= 170 && mAzimuth > 100)
+            where = "SE";
+        if (mAzimuth <= 100 && mAzimuth > 80)
+            where = "E";
+        if (mAzimuth <= 80 && mAzimuth > 10)
+            where = "NE";
+
+
+        txt_compass.setText(mAzimuth + "° " + where);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
